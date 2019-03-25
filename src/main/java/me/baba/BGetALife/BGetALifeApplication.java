892f59d65cab6b7124154b727acbb2af383a0791
kishorSingh8233 <awadh.kishor@hashworks.co>
2019-03-26 00:10:18 +0530

    Added pom.xml using springInitializer

---
+++ src/main/java/me/baba/BGetALife/BGetALifeApplication.java
@@ -0,0 +1,13 @@
+package me.baba.BGetALife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BGetALifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BGetALifeApplication.class, args);
+	}
+
+}
